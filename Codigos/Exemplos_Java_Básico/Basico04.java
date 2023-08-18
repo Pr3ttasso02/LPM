38ae27037fba6667ae7efe8add3ef2057ec1e3e7
João Caram <caram@pucminas.br>
2023-08-18 15:43:31 -0300

    Exec Basicos 1, 2, 4 em Java

--- Codigos/Exemplos_Java_Básico/Basico04.java
+++ Codigos/Exemplos_Java_Básico/Basico04.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /** 
  * MIT License
  *
@@ -21,49 +23,46 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import java.util.Scanner;
 
-public class Basico01{
-  static Scanner leitor = new Scanner(System.in);
-  
-  static int[] inverter(int vetor[]) {
-    int tmp;
-    int oposto;
-    int tamanho = vetor.length;
-    for (int i = 0; i < (tamanho/2); i++) {
-      oposto = (tamanho - i - 1);
-      tmp = vetor[i];
-      vetor[i] = vetor[oposto];
-      vetor[oposto] = tmp;
-    }
-    return vetor;
-  }
-  
-  static int[] lerVetor(int tamanho){
-    int[] vetor = new int[tamanho];
-    for (int i = 0; i < tamanho; i++) {
-      System.out.print("Digite o número na posição "+ (i + 1) +": ");
-      vetor[i] = Integer.parseInt(leitor.nextLine());
-    }
-    return vetor;
-  }
-  
-  static void imprimir(int[] vetor){
-    for (int i = 0; i < vetor.length; i++)
-      System.out.print(vetor[i]+" ");
-  }
-  
-  public static void main(String[] args) throws Exception {
-    final int TAMANHO = 6;
-    int[]  inteiros;
-      
-    inteiros = lerVetor(TAMANHO);
-    inteiros = inverter(inteiros);
-    System.out.println("\nInvertido:\n");
-    imprimir(inteiros);
-    System.out.println("\nFIM.\n");
-	leitor.close();
-  }
+public class Basico04 {
 
-  
+    final int TAMANHO_MAX = 100;
+
+    /**
+     * Inverte uma frase (string) recebida por parâmetro.
+     * @param frase Uma string com a frase a ser invertida. Pode ser vazia
+     * @return A frase invertida em uma nova String
+     */
+    static String inverter(String frase) {
+
+        String outra = "";
+        
+        int tamanho = frase.length();
+        for (int i = tamanho - 1; i >= 0; i--) {
+            outra += frase.charAt(i);
+        }
+        return outra;
+    }
+
+    static String lerFrase(Scanner teclado) {
+        String aux;
+        System.out.print("Digite uma frase de até 100 caracteres ou 'fim' para terminar: ");
+        aux = teclado.nextLine();
+        return aux;
+    }
+
+    public static void main(String[] args) {
+
+        String frase;
+        Scanner teclado = new Scanner(System.in);
+
+        frase = lerFrase(teclado);
+        while (!frase.toLowerCase().equals("fim")) {
+            frase = inverter(frase);
+            System.out.println("Invertida: " + frase);
+            frase = lerFrase(teclado);
+        }
+        teclado.close();
+    }
+
 }
